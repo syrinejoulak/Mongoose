b007c5bc6ea3b07d43fdc110d2e2d1f31067a683
Syrine Joulak <syrinejoulak@gmail.com>
2021-04-25 01:48:19 +0200

    first commit

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+module.exports = mongoose.model("UserModel", User);
+
+
+
